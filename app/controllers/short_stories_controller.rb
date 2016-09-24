fa1539859dd29d9db1d33674ff520fb45dbb4477
thomasbrockmeier <thomas.brockmeier@gmail.com>
2016-09-24 13:27:03 +0200

    basic views short story w/o tags

--- app/controllers/short_stories_controller.rb
+++ app/controllers/short_stories_controller.rb
@@ -1,2 +1,39 @@
 class ShortStoriesController < ApplicationController
+  def index
+    @short_stories = ShortStory.all
+  end
+
+  def show
+    @short_story = ShortStory.find(params[:id])
+  end
+
+  def new
+    @short_story = ShortStory.new
+  end
+
+  def create
+    @short_story = ShortStory.new(short_story_params)
+
+    if @short_story.save
+      redirect_to @short_story
+    else
+      render action: 'new'
+    end
+  end
+
+  def edit
+    @short_story = ShortStory.find(params[:id])
+  end
+
+  def update
+    short_story = ShortStory.find(params[:id])
+    short_story.update!(short_story_params)
+    redirect_to short_story
+  end
+
+
+  private
+  def short_story_params
+    params.require(:short_story).permit(:author, :title, :description, :content, :publish_date)
+  end
 end
