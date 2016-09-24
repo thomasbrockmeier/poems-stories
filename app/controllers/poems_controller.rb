078ebfd6542415ba758563876658e5bde3d80a02
thomasbrockmeier <thomas.brockmeier@gmail.com>
2016-09-24 14:28:01 +0200

    basic views poem w/o tags

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,2 +1,39 @@
 class PoemsController < ApplicationController
+  def index
+    @poems = Poem.all
+  end
+
+  def show
+    @poem = Poem.find(params[:id])
+  end
+
+  def new
+    @poem = Poem.new
+  end
+
+  def create
+    @poem = Poem.new(poem_params)
+
+    if @poem.save
+      redirect_to @poem
+    else
+      render action: 'new'
+    end
+  end
+
+  def edit
+    @poem = Poem.find(params[:id])
+  end
+
+  def update
+    poem = Poem.find(params[:id])
+    poem.update!(poem_params)
+    redirect_to poem
+  end
+
+
+  private
+  def poem_params
+    params.require(:poem).permit(:author, :title, :description, :content, :publish_date)
+  end
 end
