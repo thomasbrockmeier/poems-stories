512af4dbdd00d7db04715742672c3aa551359066
thomasbrockmeier <thomas.brockmeier@gmail.com>
2016-09-24 16:02:42 +0200

    basic form styling

--- app/assets/javascripts/form_helper.js
+++ app/assets/javascripts/form_helper.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
   $('select').material_select();
+
+  $('.datepicker').pickadate({
+    format: 'dd/mm/yyyy',
+    selectMonths: true,
+    selectYears: 100
+  });
 });
