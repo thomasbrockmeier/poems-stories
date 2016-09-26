1ed6dabf298d56cad16efd3af36a99cda41f1efe
thomasbrockmeier <thomas.brockmeier@gmail.com>
2016-09-26 11:16:09 +0200

    create authors controller and show view, automatically sort tags alphabetically

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  resources :authors
   resources :poems
   resources :short_stories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
