75d12c88b83f2cfbe48fb08f7ff50545c3ec00a8
thomasbrockmeier <thomas.brockmeier@gmail.com>
2016-09-26 16:14:19 +0200

    root to short stories

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :authors
   resources :poems
   resources :short_stories
+
+  root 'short_stories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
